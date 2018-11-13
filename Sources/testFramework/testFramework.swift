47b430115cff0543ea5052da7e3cb944044ac4aa
N.A.Shashank <shashank.na@razorpay.com>
2018-11-13 17:55:30 +0530

    testing framework

--- Sources/testFramework/testFramework.swift
+++ Sources/testFramework/testFramework.swift
@@ -2,6 +2,9 @@ import Foundation
 public struct testFramework {
     var text = "Hello, World!"
     
+    public init() {
+        // nothing to do here
+    }
     public func printText() {
         print("text is \(text)")
     }
